68d7ea3518efb0c8c7d5c6889ff4334a3aa7b7b0
Lundovskikh Kirill <borzeman@gmail.com>
2023-02-17 18:17:10 +0500

    Урок 5 из 6

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='index'),
+    path('group', views.group_post, name='group'),
+    path('group_list.html', views.group_list, name='group_list')
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,20 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    head_var = "Это главная страница проекта Yatube"
+    context = {
+        'head_var': head_var
+    }
+    return render(request, template, context)
 
-def group_posts(request):
-    return HttpResponse('Список групп')
+def group_post(request):
+    template = 'posts/group_post.html'
+    head_var = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'head_var': head_var
+    }
+    return render(request, template, context)
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
